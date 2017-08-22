c64d894416e632b478d26f6c41c25d38a3bd441c
Piotr Tarasewicz <tarasewiczp@gmail.com>
2017-08-22 20:09:43 +0200

    namespace re-frame event ids

--- notodo/src/notodo/core.cljs
+++ notodo/src/notodo/core.cljs
@@ -5,20 +5,20 @@
 
 (enable-console-print!)
 
-(rf/reg-event-db :initialise-db
+(rf/reg-event-db ::initialise-db
   (fn [_ _]
     {:todos (sorted-map-by >)
      :next-id 0}))
 
-(rf/reg-sub :get-todos
+(rf/reg-sub ::get-todos
   (fn [db _]
     (vals (:todos db))))
 
-(rf/reg-sub :get-todo
+(rf/reg-sub ::get-todo
   (fn [db id]
     (-> db :todos id)))
 
-(rf/reg-event-fx :add-todo
+(rf/reg-event-fx ::add-todo
   (fn [{:keys [db]} [_ content opts]]
     (let [id (:next-id db)
           editing? (:editing? opts)
@@ -30,15 +30,15 @@
              (update :todos assoc id todo)
              (update :next-id inc))})))
 
-(rf/reg-sub :get-property
+(rf/reg-sub ::get-property
   (fn [db [id key]]
     (get-in db [:todos id key])))
 
-(rf/reg-event-fx :set-property
+(rf/reg-event-fx ::set-property
   (fn [{:keys [db]} [_ id key val]]
     {:db (assoc-in db [:todos id key] val)}))
 
-(rf/reg-event-fx :set-property-all
+(rf/reg-event-fx ::set-property-all
   (fn [{:keys [db]} [_ key val]]
     (let [old-todos (:todos db)
           new-todos (into (empty old-todos)
@@ -47,16 +47,16 @@
                                 old-todos))]
       {:db (assoc db :todos new-todos)})))
 
-(rf/reg-event-fx :delete-todo
+(rf/reg-event-fx ::delete-todo
   (fn [{:keys [db]} [_ id]]
     {:db (update db :todos dissoc id)}))
 
 (defn todo-add-button []
-  [:button {:on-click #(rf/dispatch [:add-todo "val"])}
+  [:button {:on-click #(rf/dispatch [::add-todo "val"])}
    "Add"])
 
 (defn todo-delete-button [id]
-  [:button {:on-click #(rf/dispatch [:delete-todo id])}
+  [:button {:on-click #(rf/dispatch [::delete-todo id])}
    "Delete"])
 
 (defn todo-item [{:keys [id content editing?]}]
@@ -65,15 +65,15 @@
    (if editing?
      [:input {:value content
               :on-change #(let [val (-> % .-target .-value)]
-                            (rf/dispatch [:set-property id :content val]))}]
+                            (rf/dispatch [::set-property id :content val]))}]
      [:div {:on-click (fn [_]
-                        (rf/dispatch [:set-property-all :editing? false])
-                        (rf/dispatch [:set-property id :editing? true]))}
+                        (rf/dispatch [::set-property-all :editing? false])
+                        (rf/dispatch [::set-property id :editing? true]))}
       content])
    [todo-delete-button id]])
 
 (defn todo-list []
-  (let [todos (rf/subscribe [:get-todos])]
+  (let [todos (rf/subscribe [::get-todos])]
     [:div (map todo-item @todos)]))
 
 (defn ui []
@@ -81,14 +81,14 @@
    [todo-add-button]
    [todo-list]])
 
-(rf/dispatch [:initialise-db])
+(rf/dispatch [::initialise-db])
 
 (defonce on-click-event-listener
   (.addEventListener js/document
                      "click"
                      (fn [evt]
                        (when-not (gdom/getAncestorByClass (.-target evt) "todo-item" 3)
-                         (rf/dispatch [:set-property-all :editing? false])))))
+                         (rf/dispatch [::set-property-all :editing? false])))))
 
 (r/render [ui] (. js/document (getElementById "app")))
 
