1d6a04ee171569cea51a1522312c5cf9bb30b1ad
Piotr Tarasewicz <tarasewiczp@gmail.com>
2017-08-23 15:46:58 +0200

    idispatch :nitialize-db in notodo.core

--- notodo/src/notodo/core.cljs
+++ notodo/src/notodo/core.cljs
@@ -1,8 +1,12 @@
 (ns notodo.core
   (:require [notodo.ui :as ui]
-            [reagent.core :as r]))
+            [notodo.events :as events]
+            [reagent.core :as r]
+            [re-frame.core :as rf]))
 
 (enable-console-print!)
+
+(rf/dispatch [::events/initialise-db])
 
 (r/render [ui/ui] (. js/document (getElementById "app")))
 
