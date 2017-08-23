44b73dc6c8e5faf8531af59b980981921d8355e2
Piotr Tarasewicz <tarasewiczp@gmail.com>
2017-08-23 18:00:52 +0200

    extract components

--- notodo/src/notodo/ui.cljs
+++ notodo/src/notodo/ui.cljs
@@ -15,20 +15,26 @@
   [:button {:on-click #(rf/dispatch [::events/delete-todo id])}
    "Delete"])
 
-(defn todo-item [{:keys [::db/id ::db/content ::db/editing?]}]
+(defn todo-item-edited [{:keys [::db/id ::db/content]}]
+  [:input {:class "todo-item-content"
+           :value content
+           :placeholder placeholder
+           :on-change #(let [val (-> % .-target .-value)]
+                         (rf/dispatch [::events/set-property id ::db/content val]))}])
+
+(defn todo-item-not-edited [{:keys [::db/id ::db/content]}]
+  [:div {:class "todo-item-content"
+         :on-click (fn [_]
+                     (rf/dispatch [::events/set-edited id]))}
+   (or (seq content) placeholder)])
+
+(defn todo-item [{:keys [::db/id ::db/editing?] :as todo}]
   (let [edited? (rf/subscribe [::events/edited? id])]
     [:div {:key (str "todo-" id)
            :class "todo-item"}
      (if @edited?
-       [:input {:class "todo-item-content"
-                :value content
-                :placeholder placeholder
-                :on-change #(let [val (-> % .-target .-value)]
-                              (rf/dispatch [::events/set-property id ::db/content val])) }]
-       [:div {:class "todo-item-content"
-              :on-click (fn [_]
-                          (rf/dispatch [::events/set-edited id]))}
-        (or (seq content) placeholder)])
+       [todo-item-edited todo]
+       [todo-item-not-edited todo])
      [:div {:class "todo-item-sidebar"}
       [todo-delete-button id]]]))
 
