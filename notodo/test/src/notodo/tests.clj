dda45d21a728c092329e3dbb2b0d921fd5ab3491
Piotr Tarasewicz <tarasewiczp@gmail.com>
2017-08-26 21:47:03 +0200

    add example test

--- notodo/test/src/notodo/tests.clj
+++ notodo/test/src/notodo/tests.clj
@@ -1,7 +1,7 @@
 (ns notodo.tests
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :as t]
             [clojure.java.io :as io])
-  (:import (org.openqa.selenium WebDriver By)
+  (:import (org.openqa.selenium WebDriver By TimeoutException)
            (org.openqa.selenium.chrome ChromeDriverService ChromeDriverService$Builder)
            (org.openqa.selenium.remote DesiredCapabilities RemoteWebDriver)
            (org.openqa.selenium.support.ui ExpectedCondition)
@@ -21,15 +21,30 @@
   (RemoteWebDriver. (.getUrl service)
                     (DesiredCapabilities/chrome)))
 
-(defn run-web-tests []
-  (let [chrome-service (make-and-start-chrome-service)
-        chrome-web-driver (make-chrome-web-driver chrome-service)]
-    (.get chrome-web-driver "http://localhost:3449")
-    (. (WebDriverWait. chrome-web-driver 10)
-       (until (reify ExpectedCondition
-                (apply [_ driver]
-                  (seq (.findElements driver (By/cssSelector "#app > *")))))))
-    (.quit chrome-web-driver)))
+(def ^:dynamic *chrome-web-driver* nil)
 
-;(run-web-tests)
+(defn setup-chrome-web-driver [f]
+  (let [chrome-service (make-and-start-chrome-service)]
+    (binding [*chrome-web-driver* (make-chrome-web-driver chrome-service)]
+      (.get *chrome-web-driver* "http://localhost:3449")
+      (try (. (WebDriverWait. *chrome-web-driver* 10)
+              (until (reify ExpectedCondition
+                       (apply [_ driver]
+                         (seq (.findElements driver (By/cssSelector "#app > *")))))))
+           (f)
+           (catch TimeoutException e
+             (throw (ex-info "Unable to mount app. Is FigWheel running?" {}))))
+      *chrome-web-driver*)))
 
+(defn teardown-chrome-web-driver [chrome-web-driver]
+  (.quit chrome-web-driver))
+
+(t/use-fixtures :once #(-> % setup-chrome-web-driver teardown-chrome-web-driver))
+
+;; example test
+(t/deftest test-elements-after-startup
+  (t/testing "presence of at least one todo item"
+    (t/is (.findElement *chrome-web-driver* (By/className "todo-item")))))
+
+(defn run-tests []
+  (t/run-tests 'notodo.tests))
