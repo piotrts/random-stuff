d068f64e227e11783a48b6304338f994f8f20196
Piotr Tarasewicz <tarasewiczp@gmail.com>
2017-08-26 20:38:35 +0200

    wait until there is "app" element

--- notodo/test/src/notodo/tests.clj
+++ notodo/test/src/notodo/tests.clj
@@ -1,7 +1,11 @@
 (ns notodo.tests
-  (:require [clojure.java.io :as io])
-  (:import (org.openqa.selenium.chrome ChromeDriverService ChromeDriverService$Builder)
-           (org.openqa.selenium.remote DesiredCapabilities RemoteWebDriver)))
+  (:require [clojure.test :refer [deftest is]]
+            [clojure.java.io :as io])
+  (:import (org.openqa.selenium WebDriver By)
+           (org.openqa.selenium.chrome ChromeDriverService ChromeDriverService$Builder)
+           (org.openqa.selenium.remote DesiredCapabilities RemoteWebDriver)
+           (org.openqa.selenium.support.ui ExpectedCondition)
+           (org.openqa.selenium.support.ui WebDriverWait)))
 
 (def chrome-executable-path "/opt/chromedriver/chromedriver") ;; yeah, its hardcoded
 
@@ -21,6 +25,10 @@
   (let [chrome-service (make-and-start-chrome-service)
         chrome-web-driver (make-chrome-web-driver chrome-service)]
     (.get chrome-web-driver "http://localhost:3449")
+    (. (WebDriverWait. chrome-web-driver 10)
+       (until (reify ExpectedCondition
+                (apply [_ d]
+                  (.findElement d (By/id "app"))))))
     (.quit chrome-web-driver)))
 
 ;(run-web-tests)
